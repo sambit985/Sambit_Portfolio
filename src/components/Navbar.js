a01d6d0a5dd4295ebcb576bdd829bd36ac9a6a1e
sambit985 <kumarsambit678@gmail.com>
2023-11-11 20:40:40 +0530

    Commit 3rd time with build foler :rocket:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
     return (
         <div className={color ? "header header-bg" : "header"}>
             <Link to='/'>
-                <h1>Protofolio</h1>
+                <h1>SambitNayak</h1>
             </Link>
             <ul className={click ? "nav-menu active" : "nav-menu"}>
                 <li>
